2607b956e1d28c8bc0564d4b4950efbc9a3a3126
João Ricardo <joaobarbosa226@gmail.com>
2024-10-15 23:37:00 -0300

    preguiça

--- desafio-5/app/layout.tsx
+++ desafio-5/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Desafio 5: Top vendas",
@@ -9,6 +10,16 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
   return (
     <html lang="en">
       <body>
+        <header className="flex-row, w-full, h-[80px]">
+          <div className="flex-row, justify-between, items-center">
+            <Image 
+            src={"/images/Frame 4.png"}
+            alt=""
+            width={130}
+            height={52}/>
+            
+          </div>
+        </header>
         {children}
       </body>
     </html>
